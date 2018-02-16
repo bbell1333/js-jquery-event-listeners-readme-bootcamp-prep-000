6f79a8ffd74c7ae48eae1a91f8deaec15d543b6c
Bryan Bell <bbell1333@gmail.com>
2018-02-16 17:18:59 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -14,8 +14,8 @@ function frameIt(){
 }
 
 function pressIt(){
-  $('#typing').on('keydown',function(e){
-    if (e.keyCode == 71){
+  $('#typing').on('keydown',function(key){
+    if (key.which == 71){
       alert('g was pressed');
       return;
     }
