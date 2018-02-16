e7e83c3f70d9cd261c037f9a26a4e946edb1c62e
Bryan Bell <bbell1333@gmail.com>
2018-02-16 15:49:52 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -11,7 +11,7 @@ function frameIt(){
 }
 
 function pressIt(){
-  $('input #typing').on('keydown',function(key){
+  $('form').on('keydown',function(key){
     if (key.which == 71){
       alert('g was pressed');
       return;
